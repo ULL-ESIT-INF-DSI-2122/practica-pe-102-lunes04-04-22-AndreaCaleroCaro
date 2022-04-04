2539db1072af5de65e99aa776d512b94a613e455
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-04-04 08:32:53 +0000

    Test setStrategy() class Solver TDD

---
+++ src/strategy.ts
@@ -0,0 +1,7 @@
+/**
+ * EJERCICIO PE 102. Lunes 04 de abril 2022
+ * @interface Strategy que implementa la interfaz Strategy
+ */
+export interface Strategy {
+    execute(data: number[]): void;
+}
--- tests/ejercicio1.spec.ts
+++ tests/ejercicio1.spec.ts
@@ -1,17 +1,11 @@
 import 'mocha';
 import {expect} from 'chai';
-import {add} from '../src/ejercicio1';
+import {Solver} from '../src/solver';
+import {Strategy} from '../src/strategy';
 
-describe('add function tests', () => {
-  it('add(1, 8) returns value 9', () => {
-    expect(add(1, 8)).to.be.equal(9);
-  });
 
-  it('add(-1, 8) returns value 7', () => {
-    expect(add(-1, 8)).to.be.equal(7);
-  });
-
-  it('add(1.2, 3.5) returns value 4.7', () => {
-    expect(add(1.2, 3.5)).to.be.equal(4.7);
+describe('Solver', () => {
+  it('setStrategy(strategy: Strategy) añade el patrón', () => {
+    expect(setStrategy([5,3,4,1,2,7,6])).to.be.equal(undefined);
   });
 });
