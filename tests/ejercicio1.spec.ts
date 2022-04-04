989c22ca721562e3f63174bfdc425d96c5ed4beb
Andrea Calero Caro <alu0101202952@ull.edu.es>
2022-04-04 09:02:26 +0000

    Test execute() class MergeSort TDD

--- tests/ejercicio1.spec.ts
+++ tests/ejercicio1.spec.ts
@@ -1,13 +1,21 @@
 import 'mocha';
 import {expect} from 'chai';
 import {BubbleSort} from '../src/bubblesort';
+import {MergeSort} from '../src/mergesort';
 
 const data: number[] = [5,4,6,7,2,1,3]
 const bubblesort = new BubbleSort();
+const mergesort = new MergeSort()
 
 describe('BubbleSort algoritmo ordenacion', () => {
   it('execute() ejecuta el algoritmo de ordenacion', () => {
     expect(bubblesort.execute(data)).to.be.eql([ 1, 2, 3, 4, 5, 6, 7 ]);
+  });
+});
+
+describe('MergeSort algoritmo ordenacion', () => {
+  it('execute() ejecuta el algoritmo de ordenacion', () => {
+    expect(mergesort.execute(data)).to.be.eql([ 1, 2, 3, 4, 5, 6, 7 ]);
   });
 });
 
